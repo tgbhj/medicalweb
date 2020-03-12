df6e15067e181639b6661e88e08fad7900bf2bb3
tgbhj <j1060381781@qq.com>
2020-03-12 17:21:25 +0800

    commit

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,48 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Table } from 'antd'
 import fly from 'flyio'
 
-class Admin extends React.Component {
-    state = {
-        info: []
-    }
+function Admin() {
+    const [info, setInfo] = useState([])
 
-    async componentDidMount() {
-        await fly
-            .get('/info')
-            .then(res => {
-                if (res.data.code === 20000) {
-                    this.setState({ info: res.data.cb })
-                } else {
+    useEffect(() => {
+        const fetchData = async () => {
+            await fly
+                .get('/info')
+                .then(res => {
+                    if (res.data.code === 20000) {
+                        setInfo(res.data.cb)
+                    } else {
+                        notification.error({
+                            message: 'Error',
+                            description: '',
+                            duration: 2
+                        })
+                    }
+                })
+                .catch(err => {
                     notification.error({
-                        message: 'Error',
+                        message: err,
                         description: '',
                         duration: 2
                     })
-                }
-            })
-            .catch(err => {
-                notification.error({
-                    message: err,
-                    description: '',
-                    duration: 2
                 })
-            })
-    }
+        }
+        fetchData()
+    }, [])
 
-    render() {
-        return <Table dataSource={this.state.info} rowKey='_id' scroll={{ scrollToFirstRowOnChange: true }}
-            pagination={{ simple: true, total: this.state.info.length, pageSize: 20 }}
-            expandable={{
-                expandedRowRender: record => <p style={{ margin: 0 }}>{`联系人留言：${record.msg}`}</p>
-            }}>
-            <Table.Column title='公司名称' dataIndex='company' key='company' />
-            <Table.Column title='联系人姓名' dataIndex='name' key='name' />
-            <Table.Column title='联系人电话' dataIndex='phone' key='phone' />
-            <Table.Column title='联系人邮箱' dataIndex='email' key='email' />
-            <Table.Column title='提交日期时间' dataIndex='date' key='date' />
-        </Table>
-    }
+    return <Table dataSource={info} rowKey='_id' scroll={{ scrollToFirstRowOnChange: true }}
+        pagination={{ simple: true, total: info.length, pageSize: 20 }}
+        expandable={{
+            expandedRowRender: record => <p style={{ margin: 0 }}>{`联系人留言：${record.msg}`}</p>
+        }}>
+        <Table.Column title='公司名称' dataIndex='company' key='company' />
+        <Table.Column title='联系人姓名' dataIndex='name' key='name' />
+        <Table.Column title='联系人电话' dataIndex='phone' key='phone' />
+        <Table.Column title='联系人邮箱' dataIndex='email' key='email' />
+        <Table.Column title='提交日期时间' dataIndex='date' key='date' />
+    </Table>
 }
 
 export default Admin
--- src/components/Mask.jsx
+++ src/components/Mask.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Row, Col, Button, Modal, Carousel } from 'antd'
 import img_1 from '../images/img_1.jpg'
 import img_2 from '../images/img_2.jpg'
@@ -12,59 +12,55 @@ import img_9 from '../images/img_9.jpg'
 import img_10 from '../images/img_10.jpg'
 import Info from './Info'
 
-class Mask extends React.Component {
-    state = {
-        visible: false
-    }
-
-    render() {
-        return <Row style={{ paddingTop: 10 }} align='middle' justify='center'>
-            <Col style={{ paddingBottom: 10 }}>
-                <Button type='danger' size='large' onClick={() => this.setState({ visible: true })}>Please fill in your contact information</Button>
-                <Modal visible={this.state.visible} footer={null} onCancel={() => this.setState({ visible: false })}>
-                    <Row type='flex' justify='center' align='middle'>
-                        <Col xs={24} sm={24} md={24} lg={16} xl={16} xxl={16}>
-                            <Info />
-                        </Col>
-                    </Row>
-                </Modal>
-            </Col>
-            <Col span={22}>
-                <Carousel autoplay>
-                    <div>
-                        <img src={img_1} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_2} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_3} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_4} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_5} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_6} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_7} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_8} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_9} width='100%' height='100%' />
-                    </div>
-                    <div>
-                        <img src={img_10} width='100%' height='100%' />
-                    </div>
-                </Carousel>
-            </Col>
-        </Row>
-    }
+function Mask() {
+    const [visible, setVisible] = useState(false)
+    return < Row style={{ paddingTop: 10 }
+    } align='middle' justify='center' >
+        <Col style={{ paddingBottom: 10 }}>
+            <Button type='danger' size='large' onClick={() => setVisible(true)}>Please fill in your contact information</Button>
+            <Modal visible={visible} footer={null} onCancel={() => setVisible(false)}>
+                <Row type='flex' justify='center' align='middle'>
+                    <Col xs={24} sm={24} md={24} lg={16} xl={16} xxl={16}>
+                        <Info />
+                    </Col>
+                </Row>
+            </Modal>
+        </Col>
+        <Col span={22}>
+            <Carousel autoplay>
+                <div>
+                    <img src={img_1} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_2} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_3} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_4} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_5} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_6} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_7} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_8} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_9} width='100%' height='100%' />
+                </div>
+                <div>
+                    <img src={img_10} width='100%' height='100%' />
+                </div>
+            </Carousel>
+        </Col>
+    </Row >
 }
 
 export default Mask

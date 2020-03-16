8faa1a555f6d9c5d325b40abbbd3cdf02b1aaffd
tgbhj <j1060381781@qq.com>
2020-03-16 13:59:26 +0800

    commit

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Result, Button } from 'antd'
 
 function NotFound() {
-    return <Result status="404" title="404" subTitle="页面不存在" extra={<Button type="primary" href='/'>Back Home</Button>} />
+    return <Result status="404" title="404" subTitle="This page is not exist" extra={<Button type="primary" href='/'>Back Home</Button>} />
 }
 
 export default NotFound
